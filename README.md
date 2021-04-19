ba47b854c1944e86c539f7481af5182071fa2c97
Joshua Bloom <joshbloom@google.com>
2021-04-19 13:07:23 -0700

    Trying to add a task from CR service
    Not triggering workflow correctly yet.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rgoldberg
+
+1. Need to add python html templating system to web-frontend
+1. Dispatch to cloud tasks from the web-frontend a request for a character and track the character position
--- web-frontend/app.py
+++ web-frontend/app.py
@@ -1,17 +1,60 @@
 from flask import Flask, request
+from google.cloud import tasks_v2
+import json
 
 app = Flask(__name__)
+
+# Create a client.
+client = tasks_v2.CloudTasksClient()
+
+project = "proj2-310921"
+queue = "my-queue"
+location = "us-central1"
+workflowName = "workflow-1"
+url = (
+    "https://workflowexecutions.googleapis.com/v1/projects/"
+    + project
+    + "/locations/us-central1/workflows/"
+    + workflowName
+    + "/executions"
+)
+service_account_email = "proj2-310921@appspot.gserviceaccount.com"
+payload = "hello"
 
 
 @app.route("/", methods=["GET"])
 def hello():
     """ Return a friendly HTTP greeting. """
     who = request.args.get("who", "World")
+    dispatchToCloudTasks()
     return f"Hello {who}!\n"
 
-def dispatchToCloudTasks():
-    return
 
+def dispatchToCloudTasks():
+    # Construct the fully qualified queue name.
+    parent = client.queue_path(project, location, queue)
+
+    # Construct the request body.
+    task = {
+        "http_request": {  # Specify the type of request.
+            "http_method": tasks_v2.HttpMethod.POST,
+            "url": url,  # The full url path that the task will be sent to.
+            "oauth_token": {"service_account_email": service_account_email},
+        }
+    }
+
+    if payload is not None:
+        # The API expects a payload of type bytes.
+        converted_payload = payload.encode()
+
+        # Add the payload to the request.
+        task["http_request"]["body"] = converted_payload
+
+    # Use the client to build and send the task.
+    response = client.create_task(request={"parent": parent, "task": task})
+
+    print("Created task {}".format(response.name))
+    return response
 
 
 if __name__ == "__main__":
