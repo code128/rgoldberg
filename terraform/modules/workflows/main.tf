5ba9f0f3bafeb37f2eb6c84c6574428165795796
Joshua Bloom <code128@users.noreply.github.com>
2021-12-28 11:16:59 -0800

    Now deploying a workflow correctly.

--- terraform/modules/workflows/main.tf
+++ terraform/modules/workflows/main.tf
@@ -1,23 +1,3 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-# provider "google" {
-#   project = var.project_id
-# }
-
 # [START workflows_api_enable]
 resource "google_project_service" "workflows" {
   service            = "workflows.googleapis.com"
@@ -27,45 +7,17 @@ resource "google_project_service" "workflows" {
 
 # [START workflows_serviceaccount_create]
 resource "google_service_account" "workflows_service_account" {
-  account_id   = "sample-workflows-sa"
-  display_name = "Sample Workflows Service Account"
+  account_id   = "workflows-sa"
+  display_name = "Workflows Service Account"
 }
 # [END workflows_serviceaccount_create]
 
 # [START workflows_workflow_deploy]
-resource "google_workflows_workflow" "workflows_example" {
-  name            = "sample-workflow"
-  region          = "us-central1"
-  description     = "A sample workflow"
+resource "google_workflows_workflow" "workflows" {
+  name            = "random-service-chooser"
+  description     = "Chooses from a set of random services"
   service_account = google_service_account.workflows_service_account.id
-  source_contents = <<-EOF
-  # This is a sample workflow, feel free to replace it with your source code
-  #
-  # This workflow does the following:
-  # - reads current time and date information from an external API and stores
-  #   the response in CurrentDateTime variable
-  # - retrieves a list of Wikipedia articles related to the day of the week
-  #   from CurrentDateTime
-  # - returns the list of articles as an output of the workflow
-  # FYI, In terraform you need to escape the $$ or it will cause errors.
-
-  - getCurrentTime:
-      call: http.get
-      args:
-          url: https://us-central1-workflowsample.cloudfunctions.net/datetime
-      result: CurrentDateTime
-  - readWikipedia:
-      call: http.get
-      args:
-          url: https://en.wikipedia.org/w/api.php
-          query:
-              action: opensearch
-              search: $${CurrentDateTime.body.dayOfTheWeek}
-      result: WikiResult
-  - returnOutput:
-      return: $${WikiResult.body[1]}
-EOF
-
+  source_contents = file("../../../workflow-random-chooser/random-service-choice.yaml")
   depends_on = [google_project_service.workflows]
 }
 # [END workflows_workflow_deploy]
