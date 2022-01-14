070da972d33311375c9bae2fa8b03a8ea6dd07d3
Joshua Bloom <code128@users.noreply.github.com>
2022-01-13 17:12:21 -0800

    Updated the AE example

--- ae-random-letter/server.js
+++ ae-random-letter/server.js
@@ -19,10 +19,29 @@
 const express = require('express');
 const app = express();
 
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz123456789";
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[] ^_`{|}~";
 
 app.get('/', (req, res) => {
-  res.send(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  if (!req.query.intendedCharacter) { //Send a random character if there's no passed in character intended
+    res.send(alphabet[Math.floor(Math.random() * alphabet.length)])
+  }
+
+  if (!req.query.executionID) {req.query.executionID = "00000"};
+
+  var match = false;
+  var random_letter;
+  var iterationCounter = 0;
+  while (!match) {
+      iterationCounter++;
+      random_letter = alphabet[Math.floor(Math.random() * alphabet.length)]
+      if (random_letter ==  req.query.intendedCharacter) {
+          match = true;
+      }
+  }
+  res.json( {"executionID":req.query.executionID,
+              "intendedCharacter":req.query.intendedCharacter, 
+              "randomCharacter":random_letter, 
+              "iterationCount":iterationCounter}); 
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
